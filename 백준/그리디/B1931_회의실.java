ac8bdc769da1b4ab087d96a5964729fbc4f57628
will kim <78planet@gmail.com>
2023-03-10 18:03:27 +0900

    add greedy algorithm.

---
+++ 백준/그리디/B1931_회의실.java
@@ -0,0 +1,46 @@
+package 백준.그리디;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class B1931_회의실 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+
+        int[][] time = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            time[i][0] = Integer.parseInt(st.nextToken());
+            time[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(time, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[1] == o2[1]) {
+                    return o1[0] - o2[0];
+                }
+
+                return o1[1] - o2[1];
+            }
+        });
+
+        int cnt = 0;
+        int prevEndTime = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (prevEndTime <= time[i][0]) {
+                cnt++;
+                prevEndTime = time[i][1];
+            }
+        }
+        System.out.println(cnt);
+    }
+}
